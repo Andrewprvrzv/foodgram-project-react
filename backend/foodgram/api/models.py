7b0e6918b0fa16ad503b970063826e421e8011b4
Andrew <lvN7yHB*s_I7y2jJ@+ZrgI_a#Ie1L7*%>
2023-04-20 20:59:59 +1000

    model full

--- backend/foodgram/api/models.py
+++ backend/foodgram/api/models.py
@@ -2,37 +2,9 @@ from django.contrib.auth import get_user_model
 from django.core.validators import RegexValidator
 from django.db import models
 
-from backend.foodgram.api.validators import validate_nonzero
+from api.validators import validate_nonzero
 
 User = get_user_model()
-
-
-class Recipe(models.Model):
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        verbose_name='Автор',
-        help_text='Автор рецепта'
-    )
-    name = models.CharField(
-        max_length=200,
-    )
-    image = models.ImageField(upload_to='images/')
-    text = models.TextField(
-        on_delete=models.CASCADE,
-        verbose_name='Рецепт',
-        help_text='Описание рецепта'
-    )
-    cooking_time = models.PositiveIntegerField(
-        verbose_name='Время',
-        help_text='Время приготовления в минутах',
-        validators=[validate_nonzero, ]
-    )
-    tags = models.ManyToManyField(Tag)
-    ingredients = models.ManyToManyField(Ingredient)
-
-    def __str__(self):
-        return self.name
 
 
 class Tag(models.Model):
@@ -64,3 +36,105 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        'Ингредиент',
+        max_length=200,
+        unique=True
+    )
+    measurement_unit = models.CharField(
+        'Единица измерения',
+        max_length=200,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор',
+        help_text='Автор рецепта'
+    )
+    name = models.CharField(
+        max_length=200,
+    )
+    image = models.ImageField(upload_to='images/')
+    text = models.TextField(
+        verbose_name='Рецепт',
+        help_text='Описание рецепта'
+    )
+    cooking_time = models.PositiveIntegerField(
+        verbose_name='Время',
+        help_text='Время приготовления в минутах',
+        validators=[validate_nonzero, ]
+    )
+    tags = models.ManyToManyField(Tag)
+    ingredients = models.ManyToManyField(Ingredient)
+
+    def __str__(self):
+        return self.name
+
+
+class Favorites(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        verbose_name = 'Избранное'
+        verbose_name_plural = 'Избранное'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'recipe'],
+                name='unique_favorite'
+            )
+        ]
+
+
+class IngredientCount(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE
+    )
+    name = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE
+    )
+    amount = models.PositiveIntegerField(
+        'Количество',
+        validators=[validate_nonzero, ]
+    )
+
+
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name = 'Корзина'
+        verbose_name_plural = 'Корзина'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'recipe'],
+                name='unique_shopping_cart'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.user.username} - {self.recipe.name}'
--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -34,3 +34,28 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Subscribe(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='subscriber',
+        verbose_name='Подписчик'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='subscribing',
+        verbose_name='Подписан'
+    )
+
+    class Meta:
+        verbose_name = 'Подписка на авторов'
+        verbose_name_plural = 'Подписки на авторов'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='unique_subscribe'
+            )
+        ]
