ca786fc1b15b11692bbd3ed7f976847f20ff72b1
andrew <andrew@prvrzv.com>
2023-04-26 22:44:03 +1000

    add front

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -27,11 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
                  '158.160.28.61',
-                 '127.0.0.1']
+                 '127.0.0.1',
+                 'web']
 
 CSRF_TRUSTED_ORIGINS = ['http://158.160.28.61',
                         'http://127.0.0.1',
-                        'http://localhost']
+                        'http://localhost',
+                        'http://web']
 
 # Application definition
 
