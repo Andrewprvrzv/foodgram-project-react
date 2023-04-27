34736582a7d28be8f06ef0dc7d4e60430718f54c
andrew <andrew@prvrzv.com>
2023-04-27 10:34:35 +1000

    bug fix

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/images/'
+MEDIA_URL = 'images'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 # Default primary key field type
