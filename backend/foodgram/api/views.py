d9d06dd635e0fa007a70c9b902628990cbd2299d
andrew <andrew@prvrzv.com>
2023-05-01 10:36:26 +1000

    review

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -134,6 +134,21 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return RecipeCreateSerializer
 
     @action(detail=True,
+            methods=['delete'],
+            permission_classes=(IsAuthenticated,))
+    def favorite(self, request, **kwargs):
+        recipe = get_object_or_404(Recipe, pk=kwargs['pk'])
+        if not Favorites.objects.filter(user=request.user,
+                                        recipe=recipe).exists():
+            return Response({'detail': 'Рецепта не было в избранном!'
+                                       'Нечего удалять!'},
+                            status=status.HTTP_400_BAD_REQUEST)
+        Favorites.objects.filter(user=request.user,
+                                 recipe=recipe).delete()
+        return Response({'detail': 'Рецепт успешно удален из избранного.'},
+                        status=status.HTTP_204_NO_CONTENT)
+
+    @action(detail=True,
             methods=['post'],
             permission_classes=(IsAuthenticated,))
     def favorite(self, request, **kwargs):
@@ -155,18 +170,17 @@ class RecipeViewSet(viewsets.ModelViewSet):
     @action(detail=True,
             methods=['delete'],
             permission_classes=(IsAuthenticated,))
-    def favorite(self, request, **kwargs):
+    def shopping_cart(self, request, **kwargs):
         recipe = get_object_or_404(Recipe, pk=kwargs['pk'])
-        if not Favorites.objects.filter(user=request.user,
-                                        recipe=recipe).exists():
-            return Response({'detail': 'Рецепта не было в избранном!'
-                                       'Нечего удалять!'},
+        if not ShoppingCart.objects.filter(user=request.user,
+                                           recipe=recipe).exists():
+            return Response({'detail': 'Рецепта нет в списке покупок!'},
                             status=status.HTTP_400_BAD_REQUEST)
-        Favorites.objects.filter(user=request.user,
-                                 recipe=recipe).delete()
-        return Response({'detail': 'Рецепт успешно удален из избранного.'},
+        ShoppingCart.objects.filter(user=request.user,
+                                    recipe=recipe).delete()
+        return Response({'detail': 'Рецепт успешно удален из списка '
+                                   'покупок.'},
                         status=status.HTTP_204_NO_CONTENT)
-
     @action(detail=True,
             methods=['post'],
             permission_classes=(IsAuthenticated,))
@@ -183,21 +197,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
         ShoppingCart.objects.create(user=request.user, recipe=recipe)
         return Response(serializer.data,
                         status=status.HTTP_201_CREATED)
-
-    @action(detail=True,
-            methods=['delete'],
-            permission_classes=(IsAuthenticated,))
-    def shopping_cart(self, request, **kwargs):
-        recipe = get_object_or_404(Recipe, pk=kwargs['pk'])
-        if not ShoppingCart.objects.filter(user=request.user,
-                                           recipe=recipe).exists():
-            return Response({'detail': 'Рецепта нет в списке покупок!'},
-                            status=status.HTTP_400_BAD_REQUEST)
-        ShoppingCart.objects.filter(user=request.user,
-                                    recipe=recipe).delete()
-        return Response({'detail': 'Рецепт успешно удален из списка '
-                                   'покупок.'},
-                        status=status.HTTP_204_NO_CONTENT)
 
     @action(detail=False,
             methods=['get'],
