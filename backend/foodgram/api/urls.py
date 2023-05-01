78e907a68832271a4065a1487da676f0e89d0d34
andrew <andrew@prvrzv.com>
2023-05-01 21:07:47 +1000

    urls fix

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -5,9 +5,6 @@ from .views import (IngredientsViewSet, RecipeViewSet, SubscribeViewSet,
                     SubscriptionsViewSet, TagViewSet)
 
 router = DefaultRouter()
-router.register(r'users/subscriptions',
-                SubscriptionsViewSet,
-                basename='subscriptions-list')
 router.register(r'tags', TagViewSet)
 router.register(r'ingredients', IngredientsViewSet)
 router.register(r'recipes', RecipeViewSet)
@@ -16,6 +13,9 @@ urlpatterns = [
     path(r'users/<int:pk>/subscribe/', SubscribeViewSet.as_view({
         'post': 'retrieve',
         'delete': 'retrieve',
+    })),
+    path(r'users/subscriptions/', SubscriptionsViewSet.as_view({
+        'get': 'list',
     })),
     path(r'auth/', include('djoser.urls.authtoken')),
     path(r'', include('djoser.urls')),
