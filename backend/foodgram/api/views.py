e2c92f534d2ea0c24cdb8db91b33a02d77b85118
andrew <andrew@prvrzv.com>
2023-05-03 10:02:33 +1000

    fix recipe download

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -4,7 +4,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from api.filters import RecipeFilter
